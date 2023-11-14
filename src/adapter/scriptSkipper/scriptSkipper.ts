e2412ef6281f817f705aa1b712c008084ed65d19
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-14 22:41:59 +0200

    squash!

--- src/adapter/scriptSkipper/scriptSkipper.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
-export const IScriptSkipper = Symbol('IScriptSkipper');
-
-export interface IScriptSkipper {
-  isScriptSkipped(url: string): boolean;
-}
